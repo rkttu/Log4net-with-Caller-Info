9719b3aafb2cb6680d335694f64a2ef48891a53e
rkttu <rkttu@xinics.com>
2015-06-10 15:32:00 +0900

    1.0.2.1 release - add enableSignature parameter to ObtainLog extension method.

--- Log4netWithCallerInfo/ClouDeveloper.Log4net.CallerInfo/CallerInfoLogHelper.cs
+++ Log4netWithCallerInfo/ClouDeveloper.Log4net.CallerInfo/CallerInfoLogHelper.cs
@@ -13,30 +13,43 @@ namespace ClouDeveloper.Log4net.CallerInfo
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="instance">The instance.</param>
+        /// <param name="enableSignature">if set to <c>true</c> [enable signature].</param>
         /// <returns></returns>
-        public static CallerInfoLog ObtainLog<T>(this T instance)
+        public static CallerInfoLog ObtainLog<T>(
+            this T instance,
+            bool enableSignature = false)
         {
-            return new CallerInfoLog(LogManager.GetLogger(typeof(T)));
+            return new CallerInfoLog(
+                LogManager.GetLogger(typeof(T)),
+                enableSignature);
         }
 
         /// <summary>
         /// Obtains the logger.
         /// </summary>
         /// <param name="typeInfo">The type information.</param>
+        /// <param name="enableSignature">if set to <c>true</c> [enable signature].</param>
         /// <returns></returns>
-        public static CallerInfoLog ObtainLog(this Type typeInfo)
+        public static CallerInfoLog ObtainLog(
+            this Type typeInfo,
+            bool enableSignature = false)
         {
-            return new CallerInfoLog(LogManager.GetLogger(typeInfo));
+            return new CallerInfoLog(
+                LogManager.GetLogger(typeInfo),
+                enableSignature);
         }
 
         /// <summary>
         /// Obtains the log.
         /// </summary>
         /// <param name="log">The log.</param>
+        /// <param name="enableSignature">if set to <c>true</c> [enable signature].</param>
         /// <returns></returns>
-        public static CallerInfoLog ObtainLog(this ILog log)
+        public static CallerInfoLog ObtainLog(
+            this ILog log,
+            bool enableSignature = false)
         {
-            return new CallerInfoLog(log);
+            return new CallerInfoLog(log, enableSignature);
         }
     }
 }
