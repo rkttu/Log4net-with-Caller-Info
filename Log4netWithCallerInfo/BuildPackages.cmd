7c0ce0d192c96a5750e013fa30b11fd8de8d87be
rkttu <rkttu@xinics.com>
2015-06-12 15:15:14 +0900

    correct the package name

--- Log4netWithCallerInfo/BuildPackages.cmd
+++ Log4netWithCallerInfo/BuildPackages.cmd
@@ -6,6 +6,8 @@ if not exist RedistPackages\libs mkdir RedistPackages\libs
 
 set project=ClouDeveloper.Log4net.CallerInfo
 .nuget\nuget.exe pack "%project%\%project%.csproj" -Build -Symbols -IncludeReferencedProjects -OutputDirectory "RedistPackages\libs" -Properties configuration=Release
+
+set project=log4net-callerinfo
 .nuget\nuget.exe push "RedistPackages\libs\%project%.*.nupkg"
 
 start RedistPackages
